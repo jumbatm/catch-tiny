789cadc67af8cde6c2a7b1c189e36da1e0d074c2
Jonathan F <fungjys@gmail.com>
2018-04-28 15:26:33 +1000

    Remove references to MSG macro.
    from the removals in previous commit.

--- examples/sections.cpp
+++ examples/sections.cpp
@@ -7,17 +7,14 @@ TEST_CASE("Sections execute preceding lines of code.")
 
     SECTION("Increment once")
     {
-        MSG("Entering section 1");
         ++num;
     }
     SECTION("Increment again")
     {
-        MSG("Entering section 2");
         num += 2;
     }
     SECTION("Increment yet again")
     {
-        MSG("Entering section 3");
         num += 3;
         REQUIRE(num == 3);
     }
