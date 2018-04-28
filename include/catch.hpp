9bd36409422585330cd15279747500cc52bf9892
Jonathan F <fungjys@gmail.com>
2018-04-28 13:46:37 +1000

    Initial commit.
    Broken: Doesn't stop running.

--- include/catch.hpp
+++ include/catch.hpp
@@ -5,28 +5,42 @@
 #include <unordered_map>
 #include <string>
 
+// Preprocessor utilities.
 #define PP_CONCAT_(x, y) x ## y
 #define PP_CONCAT(x, y) PP_CONCAT_(x, y) // Indirection, as PP won't recursively stringise.
 
+// Generate a name for internal variables to reduce likelihood of collisions.
+#define CATCH_INTERNAL(x) catch_tiny_internal__ ## x
+
+// Force execution outside of main.
+#define CATCH_EXEC(x) static bool PP_CONCAT( b, __LINE__){(x)}
+
+// Create a new global test case object. Called by TEST_CASE.
 #define CATCH_TINY_GENERATE(FUNC_NAME, TEST_NAME, CASE_NAME) \
-    static void FUNC_NAME(); \
+    static void FUNC_NAME(TestCase*); \
     TestCase TEST_NAME(CASE_NAME, FUNC_NAME); \
-    static void FUNC_NAME()
+    CATCH_EXEC(CATCH_INTERNAL(last) = &TEST_NAME); \
+    static void FUNC_NAME(TestCase *this_)
 
-#define REQUIRE(...) Assertion::Assert(#__VA_ARGS__, __FILE__, __LINE__, __VA_ARGS__)
-
+// Public interface.
 #define TEST_CASE(x) CATCH_TINY_GENERATE( PP_CONCAT(f, __LINE__), PP_CONCAT(t, __LINE__), x)
-
-#define SECTION(...) if (true)
+#define SECTION(...) if (this_->sections++, CATCH_INTERNAL(idx) == __COUNTER__)
+#define REQUIRE(...) Assertion::Assert(#__VA_ARGS__, __FILE__, __LINE__, __VA_ARGS__)
 
 struct TestCase
 {
     static std::unordered_map<std::string, TestCase> allTestCases;
     const char *name;
-    void (*function)();
+    void (*function)(TestCase*);
+    size_t sections = 1;
 
-    TestCase(const char *name, void (*function)());
+    TestCase(const char *name, void (*function)(TestCase*));
 };
+
+namespace
+{
+    TestCase *CATCH_INTERNAL(last) = nullptr;
+}
 
 struct Assertion
 {
@@ -37,7 +51,11 @@ struct Assertion
     static void Assert(const char *exp, const char *fileName, size_t lineNumber, bool assertion);
 };
 
+extern size_t CATCH_INTERNAL(x);
+
 #ifdef CATCH_CONFIG_MAIN
+
+size_t CATCH_INTERNAL(idx) = 0;
 
 std::unordered_map<std::string, TestCase> TestCase::allTestCases;
 void Assertion::Assert(const char *exp, const char *fileName, size_t lineNumber, bool assertion)
@@ -47,22 +65,29 @@ void Assertion::Assert(const char *exp, const char *fileName, size_t lineNumber,
         throw Assertion{lineNumber, exp, fileName};
     }
 }
-TestCase::TestCase(const char *name, void (*function)()) : name(name), function(function)
+TestCase::TestCase(const char *name, void (*function)(TestCase*)) : name(name), function(function)
 {
     allTestCases.insert({std::string(name), *this});
 }
 
 int main()
 {
-    for (auto& i : TestCase::allTestCases)
-    {
+    for (auto& pair : TestCase::allTestCases)
+    {    
+        CATCH_INTERNAL(idx) = 0;
+
+        auto& testCase = pair.second;
         try
         {
-            i.second.function();
+            do
+            {
+                testCase.function(&testCase);
+                std::cout << testCase.sections << std::endl;
+            } while (testCase.sections--);
         }
         catch (Assertion& a)
         {
-            std::cout << "In test case: " << i.second.name << "\n" <<
+            std::cout << "In test case: " << testCase.name << "\n" <<
                             "\tAssertion failed: " <<  "REQUIRE(" << a.expression << ") at " << a.file << ":" << a.line << "\n";
             break;
         }
